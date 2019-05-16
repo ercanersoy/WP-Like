5304a3520ddf26805b01832857c8f528daa07e68
Ercan Ersoy <ercanersoy@ercanersoy.net>
2019-05-16 09:30:50 +0300

    Some changes of index.php
    - Changed description Turkish content to English content.
    - Add space character on author line.

--- wp-like/index.php
+++ wp-like/index.php
@@ -1,8 +1,8 @@
 <?php
 /*
 Plugin Name: WP Like
-Description: Örnek bir Wordpress eklentisi
-Author:Ercan ERSOY
+Description: Like plugin for WordPress
+Author: Ercan ERSOY
 Author URI: http://ercanersoy.net
 Version: 0.1
 License: LGPLv3
