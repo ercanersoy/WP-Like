5e74f915f9babcc515cd531b995edb082e27487e
Ercan Ersoy <ercanersoy@ercanersoy.net>
2019-05-16 13:50:13 +0300

    Some changes and addings
    - Change echo to return of wp_like_add_like_function function.
    - Posts have had like count.
    - Like button trigger added. But, like button function not yet
    implemented.

--- wp-like/index.php
+++ wp-like/index.php
@@ -7,4 +7,40 @@ Author URI: http://ercanersoy.net
 Version: 0.1
 License: LGPLv3
 */
+
+$POSTS_LIKE = array();
+$posts = get_posts(array('orderby' => 'ID'));
+
+if (get_option('POSTS_LIKE') == NULL)
+{
+   foreach($posts as $p)
+   {
+      array_unshift($POSTS_LIKE[$p->ID], intval(0));
+   }
+
+   add_option('POSTS_LIKE', $POSTS_LIKE);
+}
+else
+{
+   $GLOBALS['POSTS_LIKE'] = get_option('POSTS_LIKE');
+}
+
+add_filter('the_content', 'wp_like_add_like_function');
+
+function wp_like_add_like_function($content)
+{
+   $id = get_the_ID();
+   $like = NULL;
+
+   if($POSTS_LIKE[$id] == NULL)
+   {
+      $like = '0';
+   }
+
+   return '<p><button class="wp-like-button">LIKE (' . $like . ')</button></p><br />' . $content;
+}
+
+wp_register_script('like_button_script', plugins_url('./like_button.js', __FILE__), array('jquery'));
+wp_enqueue_script('like_button_script');
+
 ?>
---
+++ wp-like/like_button.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function () {
+   jQuery('.wp-like-button').click(function () {
+      alert('Not yet implemented!');
+   });
+});
